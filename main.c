1ff3e7cc4513756b0aba672c083f5134618dbf09
mirzemehdi <mirzemehdi@gmail.com>
2020-05-02 00:52:50 +0400

    Giving user option to run circular or clipped

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include "game.h"
+#include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
+
+	int option;
+	printf("Please, give the option value you wanna run\n");
+	printf("(1) Clipped\n");
+	printf("(2) Circular\n");
+	scanf("%d",&option);
+
+	if (option==1)
+	{
+		printf("Clipped\n");
+	}
+	else if (option==2)
+	{
+		printf("Circular\n");
+	}
+	else
+		printf("Wrong option\n");
 
 	arena ar=A_new(30,30); //boundaries of table
 	cellTable ct=CT_init(ar); //2D cell Table within arena
@@ -19,7 +37,7 @@ int main(int argc, char const *argv[])
 	//Creating game with given time and table
 	game g=G_create(100,ct);
 	//Starts the game .Repeats an algoritm in table each timeUnit(given by the user)
-	G_start(g); 
+	//G_start(g); 
 
 	return 0;
 }
